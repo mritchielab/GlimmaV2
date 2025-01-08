c8622a532b67e91e10f0335f76230f922112b93b
hasaru-k <35970468+hasaru-k@users.noreply.github.com>
2025-01-08 19:44:25 +1100

    .

--- inst/htmlwidgets/glimmaMDS.js
+++ inst/htmlwidgets/glimmaMDS.js
@@ -60,7 +60,7 @@ HTMLWidgets.widget({
         linkPlotsMDS(mdsView, eigenView);
 
         addColourMessage(x.data, mdsView, controlContainer);
-        addSavePlotButton(controlContainer, mdsView, eigenView, text="Save Plot",
+        addSavePlotButton(widget, controlContainer, mdsView, eigenView, text="Save Plot",
                           summaryText="MDS", expressionText="VAR");
 
       },
@@ -72,58 +72,70 @@ HTMLWidgets.widget({
   }
 });
 
-function addSavePlotButton(controlContainer, mdsPlot, eigenPlot) 
+function addSavePlotButton(widget, controlContainer, mdsPlot, eigenPlot) 
 {
-  var dropdownDiv = document.createElement("div");
-  dropdownDiv.setAttribute("class", "dropdown");
+  var saveContainer = document.createElement("div");
+  saveContainer.setAttribute("class", "saveContainer");
 
-  controlContainer.appendChild(dropdownDiv);
+  widget.insertBefore(saveContainer, controlContainer);
 
-  var dropdownButton = document.createElement("button");
-  dropdownButton.setAttribute("class", "save-button");
-  dropdownButton.innerHTML = "Save Plot";
+  var button = document.createElement("button");
+  button.setAttribute("class", "saveButtonMDS");
+  const DOWNLOAD_ICON = 
+  `<svg class="downloadSVG" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14.7928932,11.5 L11.6464466,8.35355339 C11.4511845,8.15829124 11.4511845,7.84170876 11.6464466,7.64644661 C11.8417088,7.45118446 12.1582912,7.45118446 12.3535534,7.64644661 L16.3535534,11.6464466 C16.5488155,11.8417088 16.5488155,12.1582912 16.3535534,12.3535534 L12.3535534,16.3535534 C12.1582912,16.5488155 11.8417088,16.5488155 11.6464466,16.3535534 C11.4511845,16.1582912 11.4511845,15.8417088 11.6464466,15.6464466 L14.7928932,12.5 L4,12.5 C3.72385763,12.5 3.5,12.2761424 3.5,12 C3.5,11.7238576 3.72385763,11.5 4,11.5 L14.7928932,11.5 Z M16,4.5 C15.7238576,4.5 15.5,4.27614237 15.5,4 C15.5,3.72385763 15.7238576,3.5 16,3.5 L19,3.5 C20.3807119,3.5 21.5,4.61928813 21.5,6 L21.5,18 C21.5,19.3807119 20.3807119,20.5 19,20.5 L16,20.5 C15.7238576,20.5 15.5,20.2761424 15.5,20 C15.5,19.7238576 15.7238576,19.5 16,19.5 L19,19.5 C19.8284271,19.5 20.5,18.8284271 20.5,18 L20.5,6 C20.5,5.17157288 19.8284271,4.5 19,4.5 L16,4.5 Z" transform="rotate(90 12.5 12)"/>
+  </svg>`;
+  button.innerHTML = DOWNLOAD_ICON;
 
-  var dropdownContent = document.createElement("div");
-  dropdownContent.setAttribute("class", "dropdown-content");
+  saveContainer.appendChild(button);
 
-  dropdownDiv.appendChild(dropdownButton);
-  dropdownDiv.appendChild(dropdownContent);
+  const dimmedBox = document.createElement("div");
+  dimmedBox.setAttribute("class", "dimmedBox");
+  saveContainer.appendChild(dimmedBox);
+
+  const saveModal = document.createElement("div");
+  saveModal.setAttribute("class", "saveModalMDS");
+  saveContainer.appendChild(saveModal);
 
   const renderButton = (plot, text, type) => {
     var saveButton = document.createElement("a");
     saveButton.setAttribute("href", "#");
     saveButton.innerText = text;
-    saveButton.onclick = function() {
+    saveButton.onclick = (e) => {
+      e.preventDefault();
       plot.toImageURL(type, scaleFactor=3).then(function (url) {
         var link = document.createElement('a');
         link.setAttribute('href', url);
         link.setAttribute('target', '_blank');
-        link.setAttribute('download', 'vega-export.' + type);
+        link.setAttribute('download', text);
         link.dispatchEvent(new MouseEvent('click'));
+        saveModal.classList.remove("show");
+        dimmedBox.classList.remove("show");
       });
     };
     return saveButton;
   }
 
-  var pngSummaryBtn = renderButton(mdsPlot, text="Summary plot (PNG)", type='png');
-  var svgSummaryBtn = renderButton(mdsPlot, text="Summary plot (SVG)", type='svg');
-  var pngExpressionBtn = renderButton(eigenPlot, text="Expression plot (PNG)", type='png');
-  var svgExpressionBtn = renderButton(eigenPlot, text="Expression plot (SVG)", type='svg');
+  var pngMDS = renderButton(mdsPlot, text="MDS plot (PNG)", type='png');
+  var svgMDS = renderButton(mdsPlot, text="MDS plot (SVG)", type='svg');
+  var pngVariance = renderButton(eigenPlot, text="Variance explained (PNG)", type='png');
+  var svgVariance = renderButton(eigenPlot, text="Variance explained (SVG)", type='svg');
 
-  dropdownContent.appendChild(pngSummaryBtn);
-  dropdownContent.appendChild(svgSummaryBtn);
-  dropdownContent.appendChild(pngExpressionBtn);
-  dropdownContent.appendChild(svgExpressionBtn);
+  saveModal.appendChild(pngMDS);
+  saveModal.appendChild(svgMDS);
+  saveModal.appendChild(pngVariance);
+  saveModal.appendChild(svgVariance);
 
-  dropdownButton.onclick = () => dropdownContent.classList.toggle("show");
+  button.onclick = () => {
+    saveModal.classList.add("show");
+    dimmedBox.classList.add("show");
+  };
   
-  // setup click to fade dropdown
   window.addEventListener("click", (event) => {
-    const container = event.target.closest(".dropdown");
-    if (container !== null) {
-      return;
+    if (event.target.classList.contains("dimmedBox")) {
+      saveModal.classList.remove("show");
+      dimmedBox.classList.remove("show");
     }
-    dropdownContent.classList.remove("show");
   });
 }
 
