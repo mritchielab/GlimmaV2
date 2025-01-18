505fc48854875561c13296ee9d54f7e4608521ff
hasaru-k <35970468+hasaru-k@users.noreply.github.com>
2025-01-19 07:54:45 +1100

    MDS code refactor

--- inst/htmlwidgets/glimmaMDS.js
+++ inst/htmlwidgets/glimmaMDS.js
@@ -1,15 +1,15 @@
 /* Stores classnames for reference in styles.css */
 const CLASSNAMES = {
-  plotContainer: "plotContainer",
-  controlContainer: "controlContainer",
-  mdsContainer: "mdsContainer",
-  eigenContainer: "eigenContainer",
-  saveContainer: "saveContainer",
-  saveButton: "saveButton",
-  dimmedBox: "dimmedBox",
-  saveModal: "saveModal",
-  show: "showMDS",
-  warningBox: "warningBox",
+  plotContainer: "glimmaMDS_plotContainer",
+  controlContainer: "glimmaMDS_controlContainer",
+  mdsContainer: "glimmaMDS_mdsContainer",
+  eigenContainer: "glimmaMDS_eigenContainer",
+  saveContainer: "glimmaMDS_saveContainer",
+  saveButton: "glimmaMDS_saveButton",
+  dimmedBox: "glimmaMDS_dimmedBox",
+  saveModal: "glimmaMDS_saveModal",
+  show: "glimmaMDS_show",
+  warningBox: "glimmaMDS_warningBox",
 };
 
 HTMLWidgets.widget({
@@ -29,6 +29,141 @@ HTMLWidgets.widget({
     const widget = document.getElementById(el.id);
     widget.appendChild(plotContainer);
     widget.appendChild(controlContainer);
+
+    // adds download button for the two plots
+    const addSavePlotButton = (controlContainer, mdsPlot, eigenPlot) =>
+    {
+      const saveContainer = document.createElement("div");
+      saveContainer.setAttribute("class", CLASSNAMES.saveContainer);
+    
+      controlContainer.appendChild(saveContainer);
+    
+      const button = document.createElement("button");
+      button.setAttribute("class", CLASSNAMES.saveButton);
+      // from assets.js
+      button.innerHTML = DOWNLOAD_ICON;
+      saveContainer.appendChild(button);
+    
+      const dimmedBox = document.createElement("div");
+      dimmedBox.setAttribute("class", CLASSNAMES.dimmedBox);
+      saveContainer.appendChild(dimmedBox);
+    
+      const saveModal = document.createElement("div");
+      saveModal.setAttribute("class", CLASSNAMES.saveModal);
+      saveContainer.appendChild(saveModal);
+    
+      const renderButton = (plot, text, type) => {
+        const saveButton = document.createElement("a");
+        saveButton.setAttribute("href", "#");
+        saveButton.innerText = text;
+        saveButton.onclick = (e) => {
+          e.preventDefault();
+          plot.toImageURL(type, scaleFactor=3).then(function (url) {
+            const link = document.createElement('a');
+            link.setAttribute('href', url);
+            link.setAttribute('target', '_blank');
+            link.setAttribute('download', text);
+            link.dispatchEvent(new MouseEvent('click'));
+            saveModal.classList.remove(CLASSNAMES.show);
+            dimmedBox.classList.remove(CLASSNAMES.show);
+          });
+        };
+        return saveButton;
+      }
+    
+      const pngMDS = renderButton(mdsPlot, text="MDS plot (PNG)", type='png');
+      const svgMDS = renderButton(mdsPlot, text="MDS plot (SVG)", type='svg');
+      const pngVariance = renderButton(eigenPlot, text="Variance explained (PNG)", type='png');
+      const svgVariance = renderButton(eigenPlot, text="Variance explained (SVG)", type='svg');
+    
+      saveModal.appendChild(pngMDS);
+      saveModal.appendChild(svgMDS);
+      saveModal.appendChild(pngVariance);
+      saveModal.appendChild(svgVariance);
+    
+      button.onclick = () => {
+        saveModal.classList.add(CLASSNAMES.show);
+        dimmedBox.classList.add(CLASSNAMES.show);
+      };
+      
+      window.addEventListener("click", (event) => {
+        if (event.target.classList.contains(CLASSNAMES.dimmedBox)) {
+          saveModal.classList.remove(CLASSNAMES.show);
+          dimmedBox.classList.remove(CLASSNAMES.show);
+        }
+      });
+    }
+    
+    // preprocess function for MDS data, to 
+    const processDataMDS = (x) =>
+    {
+      /* if there's only a single feature in an R vector,
+        it does not become an array after data transformation to JS */
+      if (!Array.isArray(x.data.features["numeric"]))
+      {
+        x.data.features["numeric"] = [ x.data.features["numeric"] ];
+      }
+      if (!Array.isArray(x.data.features["discrete"]))
+      {
+        x.data.features["discrete"] = [ x.data.features["discrete"] ];
+      }
+      // sort to get "-", " -" features loaded first
+      x.data.features["numeric"].sort();
+      x.data.features["discrete"].sort();
+    }
+    
+    // add interaction between the two plots
+    const linkPlotsMDS = (mdsView, eigenView) =>
+    {
+      // highlight variance plot when we change a signal in the MDS plot
+      mdsView.addSignalListener('x_axis', function(name, value) {
+        const externalSelectValue = parseInt(value.substring(3));
+        eigenView.signal("external_select_x", externalSelectValue);
+        eigenView.runAsync();
+      });
+      mdsView.addSignalListener('y_axis', function(name, value) {
+        const externalSelectValue = parseInt(value.substring(3));
+        eigenView.signal("external_select_y", externalSelectValue);
+        eigenView.runAsync();
+      });
+    }
+    
+    // adds a warning message if the number of colours in the palette
+    // is not sufficient for the data needing to be displayed
+    function addWarningMessage(data, mdsPlot, controlContainer)
+    {
+      const warningBox = document.createElement("div");
+      warningBox.classList.add(CLASSNAMES.warningBox);
+      controlContainer.appendChild(warningBox);
+
+      const updateWarningMessage = (data, controlContainer, mdsPlot) =>
+      {
+        const warningBox = controlContainer.getElementsByClassName(CLASSNAMES.warningBox)[0];
+        warningBox.classList.remove(CLASSNAMES.show);
+  
+        const colourBy = mdsPlot.signal("colour_by");
+        const colourScheme = mdsPlot.signal("colourscheme");
+  
+        const schemeCount = vega.scheme(colourScheme).length;
+        const colourCount = [...new Set(data.mdsData[colourBy])].length;
+  
+        if (data.continuousColour) return;
+        if (colourScheme === "plasma" || colourScheme === "viridis") return;
+        if (colourBy === "-") return;
+        if (schemeCount < colourCount) {
+          warningBox.innerHTML = `Warning: not enough distinct colours. ${schemeCount} supported.`;
+          warningBox.classList.add(CLASSNAMES.show);
+        }
+      }    
+
+      // update the warning box when colourscheme signal changes
+      mdsPlot.addSignalListener('colourscheme',
+        (_) => updateWarningMessage(data, controlContainer, mdsPlot));
+        
+      // update warning box when the colour_by signal changes
+      mdsPlot.addSignalListener('colour_by',
+        (_) => updateWarningMessage(data, controlContainer, mdsPlot));
+    }
 
     return {
 
@@ -78,7 +213,7 @@ HTMLWidgets.widget({
         eigenView.runAsync();
         linkPlotsMDS(mdsView, eigenView);
         addSavePlotButton(controlContainer, mdsView, eigenView);
-        addColourMessage(x.data, mdsView, controlContainer);
+        addWarningMessage(x.data, mdsView, controlContainer);
       },
 
       resize: function(_, _)
@@ -87,131 +222,3 @@ HTMLWidgets.widget({
     };
   }
 });
-
-function addSavePlotButton(controlContainer, mdsPlot, eigenPlot) 
-{
-  const saveContainer = document.createElement("div");
-  saveContainer.setAttribute("class", CLASSNAMES.saveContainer);
-
-  controlContainer.appendChild(saveContainer);
-
-  const button = document.createElement("button");
-  button.setAttribute("class", CLASSNAMES.saveButton);
-  // from assets.js
-  button.innerHTML = DOWNLOAD_ICON;
-  saveContainer.appendChild(button);
-
-  const dimmedBox = document.createElement("div");
-  dimmedBox.setAttribute("class", CLASSNAMES.dimmedBox);
-  saveContainer.appendChild(dimmedBox);
-
-  const saveModal = document.createElement("div");
-  saveModal.setAttribute("class", CLASSNAMES.saveModal);
-  saveContainer.appendChild(saveModal);
-
-  const renderButton = (plot, text, type) => {
-    var saveButton = document.createElement("a");
-    saveButton.setAttribute("href", "#");
-    saveButton.innerText = text;
-    saveButton.onclick = (e) => {
-      e.preventDefault();
-      plot.toImageURL(type, scaleFactor=3).then(function (url) {
-        var link = document.createElement('a');
-        link.setAttribute('href', url);
-        link.setAttribute('target', '_blank');
-        link.setAttribute('download', text);
-        link.dispatchEvent(new MouseEvent('click'));
-        saveModal.classList.remove(CLASSNAMES.show);
-        dimmedBox.classList.remove(CLASSNAMES.show);
-      });
-    };
-    return saveButton;
-  }
-
-  var pngMDS = renderButton(mdsPlot, text="MDS plot (PNG)", type='png');
-  var svgMDS = renderButton(mdsPlot, text="MDS plot (SVG)", type='svg');
-  var pngVariance = renderButton(eigenPlot, text="Variance explained (PNG)", type='png');
-  var svgVariance = renderButton(eigenPlot, text="Variance explained (SVG)", type='svg');
-
-  saveModal.appendChild(pngMDS);
-  saveModal.appendChild(svgMDS);
-  saveModal.appendChild(pngVariance);
-  saveModal.appendChild(svgVariance);
-
-  button.onclick = () => {
-    saveModal.classList.add(CLASSNAMES.show);
-    dimmedBox.classList.add(CLASSNAMES.show);
-  };
-  
-  window.addEventListener("click", (event) => {
-    if (event.target.classList.contains(CLASSNAMES.dimmedBox)) {
-      saveModal.classList.remove(CLASSNAMES.show);
-      dimmedBox.classList.remove(CLASSNAMES.show);
-    }
-  });
-}
-
-
-function processDataMDS(x)
-{
-  /* if there's only a single feature in an R vector,
-    it does not become an array after data transformation to JS */
-  if (!Array.isArray(x.data.features["numeric"]))
-  {
-    x.data.features["numeric"] = [ x.data.features["numeric"] ];
-  }
-  if (!Array.isArray(x.data.features["discrete"]))
-  {
-    x.data.features["discrete"] = [ x.data.features["discrete"] ];
-  }
-  // sort to get "-", " -" features loaded first
-  x.data.features["numeric"].sort();
-  x.data.features["discrete"].sort();
-}
-
-function linkPlotsMDS(mdsView, eigenView)
-{
-  // highlight variance plot when we change a signal in the MDS plot
-  mdsView.addSignalListener('x_axis', function(name, value) {
-    var externalSelectValue = parseInt(value.substring(3));
-    eigenView.signal("external_select_x", externalSelectValue);
-    eigenView.runAsync();
-  });
-  mdsView.addSignalListener('y_axis', function(name, value) {
-    var externalSelectValue = parseInt(value.substring(3));
-    eigenView.signal("external_select_y", externalSelectValue);
-    eigenView.runAsync();
-  });
-}
-
-function addColourMessage(data, view, container)
-{
-  const warningBox = document.createElement("div");
-  warningBox.classList.add(CLASSNAMES.warningBox);
-  // update the warning box when colourscheme signal changes
-  view.addSignalListener('colourscheme',
-    (_, value) => updateColourMessage(data, container, view, value));
-  // update warning box when the colour_by signal changes
-  view.addSignalListener('colour_by',
-    (_, value) => updateColourMessage(data, container, view, view.signal('colourscheme'))
-  );
-  container.appendChild(warningBox);
-}
-
-function updateColourMessage(data, container, view, value)
-{
-  const warningBox = container.getElementsByClassName(CLASSNAMES.warningBox)[0];
-  let schemeCount = vega.scheme(value).length;
-  let colourBy = view.signal("colour_by");
-  let colourCount = [...new Set(data.mdsData[colourBy])].length;
-  warningBox.classList.remove(CLASSNAMES.show);
-
-  if (data.continuousColour) return;
-  if (value === "plasma" || value === "viridis") return;
-  if (colourBy === "-") return;
-
-  if (schemeCount < colourCount) {
-    warningBox.innerHTML = `Warning: not enough distinct colours. ${schemeCount} supported.`;
-    warningBox.classList.add(CLASSNAMES.show);
-  }
-}
